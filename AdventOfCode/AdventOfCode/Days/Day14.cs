b66a7eb82dc3944deff1f971024ae9044d9c43ff
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2020-08-22 15:18:36 +0000

    Day14 Anpassung Part 1 und einfügen von Part 2 nach Lösung von u/SomeCynicalBastard (Sortierung der Rezepte nach Abhängigkeiten und dann durchlauf Top to Bottom)

--- AdventOfCode/AdventOfCode/Days/Day14.cs
+++ AdventOfCode/AdventOfCode/Days/Day14.cs
@@ -11,104 +11,115 @@ namespace AdventOfCode.Days
 {
     class Day14 : IDay
     {
-        DataTable recipes = new DataTable();
         readonly char[] progressIndicator = new char[] { '|', '/', '─', '\\' };
         int progressPos = 0;
+        List<Reaction> recipes = new List<Reaction>();
 
         public string Solve(string input, bool part2)
         {
-            if (part2) return "Part 2 is unavailable";
-            recipes.Columns.Add("Amount", 0.GetType());
-            recipes.Columns.Add("Result", "".GetType());
-            recipes.Columns.Add("Ingredients", new List<ReactionItem>().GetType());
-            recipes.Columns.Add("Produced", 0.GetType());
-            recipes.Columns.Add("Used", 0.GetType());
+            //if (part2) return "Part 2 is unavailable";
+            LoadRecipes(input.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries));
 
-            string[] recipeList = input.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-            BuildRecipe("1 ORE => 1 ORE");
-            foreach (var recipeLine in recipeList)
-                BuildRecipe(recipeLine);
-
-            Console.Clear();
-            Console.Write("Bitte Warten... ");
-            TraceRecipe("FUEL", 1);
-
-            StringBuilder result = new StringBuilder();
-            result.Append("Item\t");
-            result.Append("Result\t");
-            result.Append("Used/");
-            result.AppendLine("Produced");
-            foreach (DataRow recipe in recipes.Rows)
-                result.AppendLine(RecipeToString(recipe));
-            Console.Clear();
-            return result.ToString();
-        }
-
-        private void BuildRecipe(string recipeText)
-        {
-            MatchCollection recipeItems = Regex.Matches(recipeText, @"(?<Quantity>\d+) (?<Element>\w+)");
-            List<ReactionItem> items = new List<ReactionItem>();
-            for (int i = 0; i < recipeItems.Count; i++)
+            if (part2)
             {
-                Match currItem = recipeItems[i];
-                var itemInfo = new ReactionItem()
-                {
-                    Amount = int.Parse(currItem.Groups["Quantity"].Value),
-                    Name = currItem.Groups["Element"].Value
-                };
-                items.Add(itemInfo);
+                Console.Write("Berechne FUEL für 1.000.000.000.000 ORE... ");
+                ApproachOreTarget();
             }
-            var keyItem = items.Last();
-            items.RemoveAt(items.Count - 1);
-            DataRow recipe = recipes.NewRow();
-            recipe.SetField("Amount", keyItem.Amount);
-            recipe.SetField("Result", keyItem.Name);
-            recipe.SetField("Ingredients", items);
-            recipe.SetField("Produced", 0);
-            recipe.SetField("Used", 0);
-            recipes.Rows.Add(recipe);
+            else
+            {
+                Console.Write("Berechne für 1 FUEL... ");
+                recipes.Single(x => x.Name == "FUEL").Required = 1;
+                TraceRecipe();
+            }
+            Console.WriteLine();
+            return CreateOutput();
         }
 
-        private void TraceRecipe(string element, int required)
+        private void TraceRecipe()
         {
-            var elementRecipes = recipes.Select($"Result = '{element}'");
-            if (elementRecipes.Length != 1) throw new InvalidOperationException("Can't identify Recipe");
-
-
-            var reactionAmount = elementRecipes[0].Field<int>("Amount");
-            var production = elementRecipes[0].Field<int>("Produced");
-            var usage = elementRecipes[0].Field<int>("Used");
-            usage += required;
-            elementRecipes[0].SetField("Used", usage);
-
-            if (element == "ORE") return;
-
-            var ingredients = elementRecipes[0].Field<List<ReactionItem>>("Ingredients");
-
-            while (production < usage)
+            for(int i = 0; i < recipes.Count; i++)
             {
                 Console.CursorLeft--;
                 Console.Write(NextProgressChar());
-                foreach (ReactionItem ingredient in ingredients)
-                {
-                    TraceRecipe(ingredient.Name, ingredient.Amount);
-                }
-                production += reactionAmount;
-                elementRecipes[0].SetField("Produced", production);
+                Reaction item = recipes[i];
+                item.Cycles = (ulong)Math.Ceiling(item.Required / (double)item.Output);
+                if (item.Name != "ORE")
+                    foreach (var ingredient in item.Ingredients)
+                    {
+                        Console.CursorLeft--;
+                        Console.Write(NextProgressChar());
+                        Reaction ingredientItem = recipes.Single(x => ingredient.Key == x.Name);
+                        ingredientItem.Required += item.Cycles * ingredient.Value;
+                    }
             }
         }
 
-        private string RecipeToString(DataRow recipe)
+        private void ApproachOreTarget()
         {
-            StringBuilder result = new StringBuilder();
-            result.Append(recipe.Field<string>("Result"));
-            result.Append("\t|");
-            result.Append(recipe.Field<int>("Amount"));
-            result.Append("\t|");
-            result.Append(recipe.Field<int>("Used"));
-            result.Append("/");
-            result.Append(recipe.Field<int>("Produced"));
-            return result.ToString();
+            int loadL = Console.CursorLeft;
+            int loadT = Console.CursorTop;
+            int logL = loadL + 2;
+            ulong requiredOre = 0;
+            ulong target = Convert.ToUInt64(1000000000000);
+            ulong uBound = target;
+            ulong lBound = 0;
+            while (lBound < uBound)
+            {
+                ulong mid = (uBound + lBound) / 2;
+                Reset();
+                Console.SetCursorPosition(loadL, loadT);
+                recipes.Single(x => x.Name == "FUEL").Required = mid;
+                TraceRecipe();
+                requiredOre = recipes.Single(x=>x.Name == "ORE").Required;
+                Console.SetCursorPosition(logL++, loadT);
+                if (requiredOre > target)
+                {
+                    Console.Write("D");
+                    uBound = mid;
+                }
+                else if (requiredOre < target)
+                {
+                    if (mid == lBound) return;
+                    Console.Write("U");
+                    lBound = mid;
+                }
+                else
+                    return;
+            }
+        }
+
+        public void Reset()
+        {
+            foreach (var recipe in recipes)
+            {
+                recipe.Required = 0;
+                recipe.Cycles = 0;
+            }
+        }
+
+        private void LoadRecipes(string[] recipeList)
+        {
+            Console.WriteLine("Lade Rezepte... ");
+            recipes.Add(new Reaction("1 ORE => 1 ORE"));
+            foreach (var recipeLine in recipeList)
+                recipes.Add(new Reaction(recipeLine));
+            Console.WriteLine(recipes.Count - 1 + " Rezepte geladen.");
+            recipes = new Topological(recipes).Ordered;
+        }
+
+        private string CreateOutput()
+        {
+            string result = "Name".PadRight(10) + "|" +
+                "Ergebnis".PadRight(10) + "|" +
+                "Benötigt".PadRight(20) + "|" +
+                "Produziert".PadRight(20) + "|" +
+                "Überschuss\r\n";
+            foreach (Reaction reaction in recipes)
+            {
+                result += reaction.DetailString();
+                result += "\r\n";
+            }
+            return result;
         }
 
         private char NextProgressChar()
