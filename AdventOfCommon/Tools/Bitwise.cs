d1d7ac071eb404b0586d984f51d32f90bb9cf718
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2020-11-21 16:52:42 +0100

    Added support class for Bitwise operations

---
+++ AdventOfCommon/Tools/Bitwise.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Tools
+{
+    static class Bitwise
+    {
+        public static int SetBit(int data, int bitNr, bool value)
+        {
+            int mask = 1 << bitNr;
+            data = data & ~mask;
+            if (value)
+                data = data | mask;
+            return data;
+        }
+
+        public static bool IsBitSet(int data, int bitNr)
+        {
+            return (data & (1 << bitNr)) != 0;
+        }
+    }
+}
