1f52e45dcc5df3c0080b140e1ec01f8537d6bce8
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2020-12-02 09:34:14 +0100

    Initializing AoC2020

---
+++ AoC2020/Days/Day1.cs
@@ -0,0 +1,19 @@
+﻿using AdventOfCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2020.Days
+{
+    class Day1 : DayBase
+    {
+        public override string Title => "Report Repair";
+
+        public override string Solve(string input, bool part2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
