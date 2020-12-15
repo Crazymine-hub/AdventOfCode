03937998b5e37326c3569145c210fccbfff29c52
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2020-12-15 10:07:56 +0100

    Moved Topological ordering to AoCommon for use in 2020

---
+++ AdventOfCommon/Tools/TopologicalOrder/TopoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Tools.TopologicalOrder
+{
+    public abstract class TopoItem
+    {
+        public abstract string Name { get; set; }
+        public abstract Dictionary<string, int> Dependencies { get; set; }
+    }
+}
---
+++ AdventOfCommon/Tools/TopologicalOrder/Topological.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Tools.TopologicalOrder
+{
+    public class Topological<T> where T : TopoItem
+    {
+        HashSet<string> isTraced = new HashSet<string>();
+        public List<T> Ordered { get; private set; } = new List<T>();
+
+        public Topological(List<T> items)
+        {
+            foreach (T item in items)
+                if (!isTraced.Contains(item.Name))
+                    Trace(item, items);
+            Ordered.Reverse();
+        }
+
+        private void Trace(T item, List<T>items)
+        {
+            isTraced.Add(item.Name);
+
+            foreach (var dependency in items.Where(x => item.Dependencies.ContainsKey(x.Name)))
+                if (!isTraced.Contains(dependency.Name))
+                {
+                    Trace(dependency, items);
+                }
+
+            Ordered.Add(item);
+        }
+    }
+}
