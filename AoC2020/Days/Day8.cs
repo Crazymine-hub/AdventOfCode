8f6550c04e4ce22cfea818786910aa0b0b59f0c9
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2020-12-15 12:36:22 +0100

    Added Commentarys to Day 8

--- AoC2020/Days/Day8.cs
+++ AoC2020/Days/Day8.cs
@@ -13,15 +13,19 @@ namespace AdventOfCode.Days
 
         public override string Solve(string input, bool part2)
         {
+            // Set Up Computer and execute
             AsmComputer comp = new AsmComputer();
             comp.LoadMemory(GetLines(input));
             comp.PreventDoubleExecution = true;
             int execResult = comp.Run();
             if (!part2) return "Computer Accumulator Value = " + comp.Accumulator;
 
-
+            //Part 2 only
+            //Get all executed jmp and nop instructions in reversed execution order
             List<InstructionInfo> trace = comp.Instructions.Where(x => x.ExecutionTime >= 0 && (x.Instruction == "jmp" || x.Instruction == "nop"))
                 .OrderBy(x => x.ExecutionTime).Reverse().ToList();
+
+            //Try Executing with each instruction inverted
             int listPos = 0;
             Console.WriteLine("Changing Instruction: ");
             while (listPos < trace.Count && execResult == -1)
@@ -34,11 +38,18 @@ namespace AdventOfCode.Days
                 execResult = comp.Run();
                 Console.WriteLine("-> " + execResult + " Acc: " + comp.Accumulator);
                 if (execResult == -1)
+                    //Revert Changes and increase to next item
                     InvertInstruction(trace[listPos++]);
             }
-            return "Computer Accumulator Value = " + comp.Accumulator;
+
+            if (execResult == 0)
+                return "Computer Accumulator Value = " + comp.Accumulator;
+            else return "No instruction ran successfull.";
         }
 
+        /// <summary>
+        /// Inverts a JMP or NOP Instruction to the other.
+        /// </summary>
         private void InvertInstruction(InstructionInfo instruction)
         {
             if (instruction.Instruction == "nop")
