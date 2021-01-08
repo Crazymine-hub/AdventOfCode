67fd96eccf58aad1e9a9cccba7c1b2489589a7a7
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2021-01-08 12:41:56 +0100

    Added Commentarys to Day 13

--- AoC2020/Days/Day13.cs
+++ AoC2020/Days/Day13.cs
@@ -23,6 +23,7 @@ namespace AdventOfCode.Days
             long time = arrival;
             rowWidth = arrival.ToString().Length + 2;
 
+            //get all specified shuttle infos
             List<ShuttleBus> shuttles = new List<ShuttleBus>();
             string[] shuttleIDs = plan[1].Split(',');
             for (int i = 0; i < shuttleIDs.Length; i++)
@@ -37,11 +38,12 @@ namespace AdventOfCode.Days
                 if (id.ToString().Length + 5 > rowWidth) rowWidth = id.ToString().Length + 7;
             }
 
-
+            //Print the header
             Console.CursorLeft = rowWidth;
             foreach (var shuttle in shuttles)
                 Console.Write("|" + ConsoleAssist.Center("Bus: " + shuttle.ID, rowWidth));
             Console.WriteLine("|");
+            //start the right routine
             if (part2)
                 return GetOrdered(shuttles);
             else
@@ -51,55 +53,60 @@ namespace AdventOfCode.Days
         private string GetOrdered(List<ShuttleBus> shuttles)
         {
             bool orderedDeparture = true;
-            long startTime = 0;
-            int reference = shuttles.Max(x => x.ID);
-            reference = shuttles.IndexOf(shuttles.Single(x => x.ID == reference));
-            reference = 0;
+            long time = 0;
+            //our reference shuttle (we need to go in the specified order, hence the constant)
+            const int reference = 0;
             List<ShuttleBus> trackedDepart = new List<ShuttleBus>();
             trackedDepart.Add(shuttles[0]);
             long trackDepartCount = 0;
             long stepSize = 1;
             rowWidth = 4;
+            //progress over time loop
             do
             {
                 orderedDeparture = true;
-                Console.Write(startTime.ToString().PadLeft(rowWidth));
+                Console.Write(time.ToString().PadLeft(rowWidth));
                 List<ShuttleBus> departed = new List<ShuttleBus>();
+                //check each Shuttle for this step
                 for (int i = 0; i < shuttles.Count; i++)
                 {
                     ShuttleBus shuttle = shuttles[i];
-                    shuttle.Departures = startTime / (long)shuttle.ID;
-                    //if (i <= reference) --shuttle.Departures;
-                    while (startTime + (long)(shuttle.Offset - shuttles[reference].Offset) > shuttle.Departures * (long)shuttle.ID)
+                    //get the departures and make sure it is not before the required Time (time + offset)
+                    shuttle.Departures = time / (long)shuttle.ID;
+                    while (time + (long)(shuttle.Offset - shuttles[reference].Offset) > shuttle.Departures * (long)shuttle.ID)
                         ++shuttle.Departures;
-                    if (startTime + (long)(shuttle.Offset - shuttles[reference].Offset) == shuttle.Departures * (long)shuttle.ID)
+                    //Check whether the shuttle departs at this time + offset timestamp (offset is always relative to our reference)
+                    if (time + (long)(shuttle.Offset - shuttles[reference].Offset) == shuttle.Departures * (long)shuttle.ID)
                     {
                         Console.Write("|" + ConsoleAssist.Center(shuttle.Offset.ToString(), rowWidth));
+                        //Create a list of all shuttles that departed in the right order
                         if (i == departed.Count)
                             departed.Add(shuttle);
                     }
                     else
-                    {
+                    {//the shuttle didn't depart as required
                         Console.Write("|" + ConsoleAssist.Center(".", rowWidth));
                         orderedDeparture = false;
                     }
                 }
-                if (departed.Count >= (trackedDepart?.Count ?? 0))
+                //check if we had another shuttle departing
+                if (departed.Count > (trackedDepart?.Count ?? 0))
                 {
                     if (departed.Except(trackedDepart).Count() == departed.Count - trackedDepart.Count)
-                    {
+                    {//adjust the step size and refresh our found departures
                         stepSize = MathHelper.LeastCommonMultiple(stepSize, trackedDepart.Last().ID);
                         Console.Write("|\t" + stepSize);
-                        trackDepartCount = startTime;
+                        trackDepartCount = time;
                         trackedDepart = departed;
                     }
                 }
                 Console.WriteLine("|");
 
-                startTime += stepSize;
+                //Go to the next time where the found shuttles depart together
+                time += stepSize;
             } while (!orderedDeparture);
 
-            return "First ordered Departure at " + (startTime + (shuttles[0].Offset - shuttles[reference].Offset) - stepSize);
+            return "First ordered Departure at " + (time + (shuttles[0].Offset - shuttles[reference].Offset) - stepSize);
         }
 
         private string GetFirstDepart(List<ShuttleBus> shuttles, long time)
@@ -108,34 +115,41 @@ namespace AdventOfCode.Days
             long departure = 0;
             List<ShuttleBus> departed = new List<ShuttleBus>();
 
+            //Iterate over time while shuttles need to depart
             do
             {
                 Console.Write(time.ToString().PadLeft(rowWidth, time == arrival ? '=' : ' '));
                 bool hasDeparture = false;
+                //iterate over the shuttles
                 for (int i = 0; i < shuttles.Count; i++)
                 {
                     ShuttleBus shuttle = shuttles[i];
+                    //check if our shuttle currently departs
                     if (time == shuttle.Departures * (long)shuttle.ID)
                     {
                         hasDeparture = true;
                         Console.Write("|" + ConsoleAssist.Center("D", rowWidth));
+                        //are we at the bus stop yet?
                         if (time >= arrival)
                         {
                             if (departed.Count == 0)
-                            {
+                            {//this is the first bus, we can catch and it's departure time
                                 firstShuttle = (long)shuttle.ID;
                                 departure = time;
                             }
+                            //track this shuttle as departed
                             departed.Add(shuttle);
                         }
                         ++shuttle.Departures;
                     }
                     else Console.Write("|" + ConsoleAssist.Center(".", rowWidth));
                 }
+                //if the time has no departure, reuse this line to save vertical screen space
                 if (!hasDeparture && time != arrival)
                     Console.CursorLeft = 0;
                 else Console.WriteLine("|");
                 time++;
+                //check if all shuttles are departed
             } while (shuttles.Except(departed).Count() > 0);
 
             return string.Format("Departing: Shuttle {0} @{1} Result: {2}", firstShuttle, departure, firstShuttle * (departure - arrival));
