12262cbb5d71dc1359dd45fab22aa58c84aee74c
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2021-02-04 11:25:52 +0100

    Origin Tracking and Downsizing for Dynamic Grid Modified Dynamic Grid to keep Track of an origin and allow accessing relative to it. Added Counting Function. Added a function to cut down on unused Borders.

--- AdventOfCommon/Tools/DynamicGrid.cs
+++ AdventOfCommon/Tools/DynamicGrid.cs
@@ -13,11 +13,14 @@ namespace AdventOfCode.Tools
         public int XDim { get; private set; }
         public int YDim { get; private set; }
 
+        public int XOrigin { get; private set; }
+        public int YOrigin { get; private set; }
+
         public DynamicGrid(int dimX = 1, int dimY = 1)
         {
             grid = new List<List<T>>();
-            XDim = dimX;
-            YDim = dimY;
+            while (XDim < dimX) IncreaseX(false);
+            while (YDim < dimY) IncreaseY(false);
         }
 
         public void IncreaseX(bool front)
@@ -25,6 +28,8 @@ namespace AdventOfCode.Tools
             for (int y = 0; y < YDim; y++)
                 grid[y].Insert(front ? 0 : XDim, default);
             ++XDim;
+            if (front)
+                ++XOrigin;
         }
 
         public void IncreaseY(bool front)
@@ -36,38 +41,62 @@ namespace AdventOfCode.Tools
             }
             grid.Insert(front ? 0 : YDim, row);
             ++YDim;
+            if (front)
+                ++YOrigin;
         }
 
         public void DecreaseX(bool front)
         {
             for (int y = 0; y < YDim; y++)
-                grid[y].RemoveAt(front ? 0 : XDim);
+                grid[y].RemoveAt(front ? 0 : XDim - 1);
             --XDim;
+            if (front)
+                --XOrigin;
         }
 
         public void DecreaseY(bool front)
         {
             grid.RemoveAt(front ? 0 : YDim - 1);
             --YDim;
+            if (front)
+                --YOrigin;
         }
 
+        public T GetRelative(int x, int y)
+        {
+            return this[XOrigin + x, YOrigin + y];
+        }
 
+        public void SetRelative(int x, int y, T value)
+        {
+            this[XOrigin + x, YOrigin + y] = value;
+        }
+
+        public void CutDown(Predicate<T> isEmpty = null)
+        {
+            if (isEmpty == null) isEmpty = new Predicate<T>(x => x.Equals(default(T)));
+
+            while (grid[0].All(x => isEmpty(x))) DecreaseY(true);
+            while (grid.Last().All(x => isEmpty(x))) DecreaseY(false);
+            while (grid.All(x => isEmpty(x[0]))) DecreaseX(true);
+            while (grid.All(x => isEmpty(x.Last()))) DecreaseX(false);
+        }
 
         public T this[int x, int y]
         {
             get => grid[y][x];
             set
             {
-                while (x > XDim) IncreaseX(false);
+                while (x >= XDim) IncreaseX(false);
                 while (x < 0)
                 {
                     IncreaseX(true);
                     ++x;
                 }
-                while (y > XDim) IncreaseY(false);
+                while (y >= YDim) IncreaseY(false);
                 while (y < 0)
                 {
-                    IncreaseX(true);
+                    IncreaseY(true);
                     ++y;
                 }
 
@@ -75,7 +104,7 @@ namespace AdventOfCode.Tools
             }
         }
 
-        public string ToString(Func<T, int, int, string> stringConverter, Func<string, string> lineEndHandler = null)
+        public string ToString(Func<T, int, int, string> stringConverter, Func<string, int, string> lineEndHandler = null)
         {
             StringBuilder result = new StringBuilder();
             for (int y = 0; y < YDim; ++y)
@@ -84,10 +113,19 @@ namespace AdventOfCode.Tools
                 for (int x = 0; x < XDim; ++x)
                     line += stringConverter(this[x, y], x, y);
                 if (lineEndHandler != null)
-                    line = lineEndHandler(line);
+                    line = lineEndHandler(line, y);
                 result.AppendLine(line);
             }
             return result.ToString().Remove(result.Length - Environment.NewLine.Length);
+        }
+
+        public long Count(Predicate<T> checker = null)
+        {
+            long result = 0;
+            for (int y = 0; y < YDim; ++y)
+                for (int x = 0; x < XDim; ++x)
+                    if (checker == null || checker(this[x, y])) ++result;
+            return result;
         }
     }
 }
