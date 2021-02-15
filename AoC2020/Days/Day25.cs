c10198199366cd1851a05075bcdb366df1093761
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2021-02-15 14:07:47 +0100

    Added Day25

---
+++ AoC2020/Days/Day25.cs
@@ -0,0 +1,30 @@
+﻿using AdventOfCode.Days.Tools.Day25;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Days
+{
+    public class Day25 : DayBase
+    {
+        public override string Title => "Combo Breaker";
+
+        RfidClient Card;
+        RfidClient Door;
+        public override string Solve(string input, bool part2)
+        {
+            if (part2) return "Part 2 is unavailoable.";
+            List<string> keys = GetLines(input);
+            Card = new RfidClient(long.Parse(keys[0]));
+            Door = new RfidClient(long.Parse(keys[1]));
+
+            long cardEnc = Card.GetEncriptionKey(Door.PublicKey);
+            long doorEnc = Door.GetEncriptionKey(Card.PublicKey);
+
+            if (cardEnc == doorEnc) return "The encription key is " + cardEnc;
+            else return "No encription key found!";
+        }
+    }
+}
---
+++ AoC2020/Days/Tools/Day25/RfidClient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Days.Tools.Day25
+{
+    class RfidClient
+    {
+        public long PublicKey { get; }
+        public int LoopCount { get; private set; }
+
+        public RfidClient(long recievedKey)
+        {
+            PublicKey = recievedKey;
+            LoopCount = 0;
+            long key = 1;
+            while(key != PublicKey)
+            {
+                key = DoStep(key, 7);
+                ++LoopCount;
+            }
+        }
+
+        private long DoStep(long value, long subject)
+        {
+            value *= subject;
+            value %= 20201227;
+            return value;
+        }
+
+        public long GetEncriptionKey(long subject)
+        {
+            long encKey = 1;
+            for (int i = 0; i < LoopCount; ++i)
+                encKey = DoStep(encKey, subject);
+            return encKey;
+        }
+    }
+}
