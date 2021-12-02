0dd40dad63dea278b60bf6627a37eb80ae19fea6
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2021-12-02 09:02:19 +0100

    Added Day2 Part 2

--- AoC2021/Days/Day2.cs
+++ AoC2021/Days/Day2.cs
@@ -13,11 +13,13 @@ namespace AdventOfCode.Days
 
         int depth = 0;
         int left = 0;
+        int aim = 0;
+        bool part2 = false;
 
 
         public override string Solve(string input, bool part2)
         {
-            if (part2) return Part2UnavailableMessage;
+            this.part2 = part2;
             List<string> instructions = GetLines(input);
             Console.WriteLine("Diving");
             foreach (string instruction in instructions)
@@ -29,22 +31,40 @@ namespace AdventOfCode.Days
         {
             Match parsed = Regex.Match(instruction, @"([a-z]+) (\d+)");
             int value = int.Parse(parsed.Groups[2].Value);
-            Console.WriteLine($"Going {parsed.Groups[1].Value} for {value} units.");
+            if (!part2)
+                Console.WriteLine($"Going {parsed.Groups[1].Value} for {value} units.");
             switch (parsed.Groups[1].Value)
             {
                 case "forward":
+                    if (part2)
+                    {
+                        Console.WriteLine($"Going for Aim {aim} and {value} units.");
+                        depth += aim * value;
+                    }
                     left += value;
                     break;
                 case "up":
+                    if (part2)
+                    {
+                        Console.WriteLine($"Aiming up {value} units.");
+                        aim -= value;
+                        break;
+                    }
                     depth -= value;
                     break;
                 case "down":
+                    if (part2)
+                    {
+                        Console.WriteLine($"Aiming down {value} units.");
+                        aim += value;
+                        break;
+                    }
                     depth += value;
                     break;
                 default:
                     throw new Exception($"Unexpected Value: {parsed.Groups[1].Value}");
             }
-            
+
         }
     }
 }
