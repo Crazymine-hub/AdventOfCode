e8d7f24f60a6a8c1bf78d7e7982ed2e23a7891bf
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2021-12-03 09:05:48 +0100

    Added Day3 Part 1

---
+++ AoC2021/Days/Day3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Days
+{
+    public class Day3 : DayBase
+    {
+        public override string Title => "Binary Diagnostic";
+
+        public override string Solve(string input, bool part2)
+        {
+            if (part2) return Part2UnavailableMessage;
+            List<string> report = GetLines(input);
+            int[] bitCount = new int[report.Max(x => x.Length)];
+            foreach (string diagValue in report)
+                CountBits(diagValue, ref bitCount);
+            int gamma = 0;
+            for (int i = 0; i < bitCount.Length; i++)
+                if (bitCount[i] > report.Count / 2)
+                    gamma = Tools.Bitwise.SetBit(gamma, bitCount.Length - 1 - i, true);
+            long epsilon = ~gamma & Tools.Bitwise.GetBitMask(bitCount.Length);
+            return $"Gamma: {gamma} Epsilon: {epsilon} Power Level: {gamma * epsilon}";
+        }
+
+        private void CountBits(string diagValue, ref int[] bitCount)
+        {
+            for (int i = 0; i < diagValue.Length; i++)
+            {
+                if (diagValue[i] == '1')
+                    ++bitCount[i];
+            }
+        }
+    }
+}
