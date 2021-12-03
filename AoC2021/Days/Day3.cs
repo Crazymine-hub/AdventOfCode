c18ea6eef9d0e17a38e1f6d0445eec30f8b26e2c
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2021-12-03 13:56:32 +0100

    Implementation for Day3 Part 2

--- AoC2021/Days/Day3.cs
+++ AoC2021/Days/Day3.cs
@@ -12,26 +12,65 @@ namespace AdventOfCode.Days
 
         public override string Solve(string input, bool part2)
         {
-            if (part2) return Part2UnavailableMessage;
             List<string> report = GetLines(input);
-            int[] bitCount = new int[report.Max(x => x.Length)];
-            foreach (string diagValue in report)
-                CountBits(diagValue, ref bitCount);
+
+            if (part2)
+                return GetLifeSupportData(report);
+            return GetPowerLevel(report);
+        }
+
+        private string GetPowerLevel(List<string> report)
+        {
+            int[] bitCount = CountBits(report);
             int gamma = 0;
             for (int i = 0; i < bitCount.Length; i++)
-                if (bitCount[i] > report.Count / 2)
+                if (IsCommonSet(report.Count, bitCount[i]))
                     gamma = Tools.Bitwise.SetBit(gamma, bitCount.Length - 1 - i, true);
             long epsilon = ~gamma & Tools.Bitwise.GetBitMask(bitCount.Length);
             return $"Gamma: {gamma} Epsilon: {epsilon} Power Level: {gamma * epsilon}";
         }
 
-        private void CountBits(string diagValue, ref int[] bitCount)
+
+        private string GetLifeSupportData(List<string> report)
         {
-            for (int i = 0; i < diagValue.Length; i++)
-            {
-                if (diagValue[i] == '1')
-                    ++bitCount[i];
-            }
+            int o2Rating = GetSystemData(true, report);
+            int co2Rating = GetSystemData(false, report);
+            return $"O2 Rating: {o2Rating} Co2 Rating: {co2Rating} {o2Rating * co2Rating}";
         }
+
+        private int GetSystemData(bool mostCommon, List<string> report)
+        {
+            List<string> searchData = report;
+            int position = 0;
+            while (searchData.Count > 1)
+            {
+                int[] bitCount = CountBits(searchData);
+                bool isCommonSet = IsCommonSet(searchData.Count, bitCount[position]);
+                searchData = searchData.Where(x => x[position] == (mostCommon ^ isCommonSet ? '0' : '1')).ToList();
+                ++position;
+            }
+
+            string resultBinary = searchData.Single();
+            int result = 0;
+            for(int i = 0; i < resultBinary.Length; ++i)
+            {
+                if (resultBinary[i] == '1')
+                    result = Tools.Bitwise.SetBit(result, resultBinary.Length - 1 - i, true);
+            }
+            return result;
+        }
+
+        private int[] CountBits(IEnumerable<string> report)
+        {
+            int[] bitCount = new int[report.Max(x => x.Length)];
+            foreach (string diagValue in report)
+                for (int i = 0; i < diagValue.Length; i++)
+                {
+                    if (diagValue[i] == '1')
+                        ++bitCount[i];
+                }
+            return bitCount;
+        }
+        private static bool IsCommonSet(int reportLength, int bitCountValue) => !(bitCountValue < reportLength / 2.0);
     }
 }
