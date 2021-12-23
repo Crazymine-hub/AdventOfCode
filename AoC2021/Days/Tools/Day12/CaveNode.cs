db5ef2119cd18aa87f2d1dcb46e18a43cd2bb993
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2021-12-23 01:37:12 +0100

    Added Day12

---
+++ AoC2021/Days/Tools/Day12/CaveNode.cs
@@ -0,0 +1,26 @@
+﻿using AdventOfCode.Tools.Pathfinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Days.Tools.Day12
+{
+    internal class CaveNode : BaseNode
+    {
+        public string Name { get; }
+        public bool Revisitable { get; }
+
+        public CaveNode(string name, bool revisitable) : base(0,0)
+        {
+            Name = name;
+            Revisitable = revisitable;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
