0a9a2ba6c65dbf1cf03fe1d4d0858e6f2526be23
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2022-12-02 19:40:21 +0100

    Added Day1 & 2

--- AoC2022/Days/Day1.cs
+++ AoC2022/Days/Day1.cs
@@ -13,8 +13,12 @@ namespace AdventOfCode.Days
 
         public override string Solve(string input, bool part2)
         {
-            if (part2) return Part2UnavailableMessage;
-            return "HelloWorld";
+            var calories = GetGroupedLines(input).Select(group => GetLines(group).Select(line => int.Parse(line)).Sum());
+
+            if(part2)
+                return $"{calories.OrderByDescending(x => x).Take(3).Sum()} Calories are carried by the top 3 elves.";
+
+            return $"{calories.Max()} Calories are the maximum carried by an elve.";
         }
     }
 }
---
+++ AoC2022/Days/Day2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Days
+{
+    public class Day2 : DayBase
+    {
+        public override string Title => "Rock Paper Scissors";
+
+
+        private readonly List<char> myShapes = new List<char> { 'X', 'Y', 'Z' };
+        private readonly List<char> oppShapes = new List<char> { 'A', 'B', 'C' };
+
+        public override string Solve(string input, bool part2)
+        {
+            int score = 0;
+            foreach (var line in GetLines(input))
+            {
+                Console.Write(line);
+                Console.Write(' ');
+                var opp = line[0];
+                var me = line[2];
+                if (part2)
+                    me = ChooseSymbol(opp, me);
+                score += myShapes.IndexOf(me) + 1;
+                switch(myShapes.IndexOf(me) - oppShapes.IndexOf(opp))
+                {
+                    case -2:
+                        Console.Write("WIN");
+                        score += 6;
+                        break;
+                    case -1:
+                        Console.Write("LOOSE");
+                        break;
+                    case 0:
+                        Console.Write("DRAW");
+                        score += 3;
+                        break;
+                    case 1:
+                        Console.Write("WIN");
+                        score += 6;
+                        break;
+                    case 2:
+                        Console.Write("LOOSE");
+                        break;
+                }
+                Console.Write(' ');
+                Console.WriteLine(score);
+            }
+
+            return $"Your Score is {score}";
+        }
+
+        private char ChooseSymbol(char opp, char outcome)
+        {
+            int oppIndex = oppShapes.IndexOf(opp);
+            switch (outcome)
+            {
+                case'X'://loose
+                    --oppIndex;
+                    break;
+                case'Y'://draw
+                    break;
+                case'Z'://win
+                    ++oppIndex;
+                    break;
+            }
+            if(oppIndex < 0) oppIndex = myShapes.Count - 1;
+            if(oppIndex >= myShapes.Count) oppIndex = 0;
+            return myShapes[oppIndex];
+        }
+    }
+}
