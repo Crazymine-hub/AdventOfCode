1128a1e47e715b29a8b3bd9e8045562510feebb8
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2022-12-02 18:11:00 +0100

    Setup for AoC2022

---
+++ AoC2022/Days/Day1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode;
+
+namespace AdventOfCode.Days
+{
+    public class Day1 : DayBase
+    {
+        public override string Title => "Calorie Counting";
+
+        public override string Solve(string input, bool part2)
+        {
+            if (part2) return Part2UnavailableMessage;
+            return "HelloWorld";
+        }
+    }
+}
