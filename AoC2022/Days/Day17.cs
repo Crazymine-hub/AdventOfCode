c7445545dd42b0bc6f0e62ab67d9a00287a185d3
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2023-03-17 23:32:12 +0100

    Updated simulation for part2

--- AoC2022/Days/Day17.cs
+++ AoC2022/Days/Day17.cs
@@ -1,6 +1,9 @@
-﻿using AdventOfCode.Tools.Visualization;
+﻿using AdventOfCode.Tools;
+using AdventOfCode.Tools.Visualization;
 using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
@@ -56,38 +59,24 @@ namespace AdventOfCode.Days
             }
         };
         private const int CaveWidth = 7;
-        HashSet<Point> occupiedPoints = new HashSet<Point>();
+        private HashSet<Point> occupiedPoints = new HashSet<Point>();
+        private ConsoleAssist ConsoleAssist = new ConsoleAssist();
 
-
-        private VisualFormHandler form = VisualFormHandler.GetInstance();
-        private const int PixelSize = 20;
-        private readonly Brush BackgroundColor = Brushes.Black;
-        private readonly Brush BorderColor = Brushes.White;
-        private readonly Brush FixedStoneColor = Brushes.Gray;
-        private readonly Brush MovableStoneColor = Brushes.Lime;
-        private readonly Brush GridColor = Brushes.DarkBlue;
-        Bitmap image;
-        Graphics g;
-        private int imageTowerHeight;
+        private long hiddenTowerHeight;
 
         public override string Solve(string input, bool part2)
         {
-            if (part2) return Part2UnavailableMessage;
             _movement = input;
-
-            form.Show();
-            Console.ReadLine();
-
-
             int shapeIndex = 0;
             int sidewaysIndex = 0;
             Point rockPosition = Point.Empty;
             List<Point> currentShape = null;
             bool movedByJet = true;
             SpawnRock(ref shapeIndex, out rockPosition, out currentShape);
-            RenderCaveSystem();
-            int rockCount = 1;
-            while (rockCount <= 2022)
+            uint rockCount = 1;
+            var maxRocks = part2 ? 1_000_000_000_000 : 2022;
+            decimal progress = -1;
+            while (rockCount <= maxRocks)
             {
                 var nextPosition = new Point(rockPosition.X, rockPosition.Y);
                 if (movedByJet)
@@ -108,16 +97,27 @@ namespace AdventOfCode.Days
                 else if (!movedByJet)
                 {
                     SetGrid(currentShape, rockPosition);
+                    HideLowerCave();
+                    //RenderCaveSystem();
                     SpawnRock(ref shapeIndex, out rockPosition, out currentShape);
+                    var newProgress = Math.Round(rockCount * 100 / (decimal)maxRocks, 3);
+                    if(newProgress > progress)
+                    {
+                        progress = newProgress;
+                        Console.CursorLeft = 0;
+                        Console.Write(progress.ToString("0.000"));
+                        Console.Write("% ");
+                        Console.Write(ConsoleAssist.GetNextProgressChar());
+                    }
                     rockCount++;
                     movedByJet = false;
-                    RenderCaveSystem();
                 }
 
                 movedByJet = !movedByJet;
             }
 
-            var towerHeight = occupiedPoints.Max(x => x.Y) + 1;
+            var towerHeight = GetTowerHeight() + hiddenTowerHeight;
+            Console.WriteLine();
             return $"The Rock tower is {towerHeight} units tall";
         }
 
@@ -126,12 +126,14 @@ namespace AdventOfCode.Days
             //spawn next rock
             var top = 0;
             if (occupiedPoints.Any())
-                top = occupiedPoints.Max(x => x.Y) + 1;
+                top = GetTowerHeight();
             top += 3;
             currentShape = _shapes[shapeIndex++];
             if (shapeIndex >= _shapes.Count) shapeIndex = 0;
             rockPosition = new Point(Math.Abs(currentShape.Min(x => x.X)) + 2, Math.Abs(currentShape.Min(x => x.Y)) + top);
         }
+
+        private int GetTowerHeight() => occupiedPoints.Max(x => x.Y) + 1;
 
         private void SetGrid(List<Point> shape, Point shapePosition)
         {
@@ -145,41 +147,24 @@ namespace AdventOfCode.Days
             {
                 var targetX = shapePosition.X + point.X;
                 var targetY = shapePosition.Y + point.Y;
+                if (targetY < 0) ;
                 if (targetX < 0 || targetX >= CaveWidth || targetY < 0 || occupiedPoints.Contains(new Point(targetX, targetY))) return false;
             }
             return true;
         }
 
-        private void RenderCaveSystem()
+        private void HideLowerCave()
         {
-            if(image == null || occupiedPoints.Max(x => x.Y) + 1 >= imageTowerHeight)
-                CreateNewImage();
-            g.FillRectangle(BackgroundColor, 0, 0, image.Width, image.Height);
-            g.FillRectangle(BorderColor, 0, 0, PixelSize, image.Height);
-            g.FillRectangle(BorderColor, image.Width - PixelSize, 0, PixelSize, image.Height);
-            g.FillRectangle(BorderColor, 0, image.Height - PixelSize, image.Width, image.Height);
+            var offset = occupiedPoints.GroupBy(x => x.X).Select(x => new Point(x.Key, x.Max(p => p.Y))).Min(x => x.Y);
+            if (offset == 0) return;
+            //var oldHeight = GetTowerHeight() + hiddenTowerHeight;
+            //Console.WriteLine($"Height: {oldHeight}");
+            //var toRemove = occupiedPoints.Where(p => p.Y < hiddenHeightStep).ToHashSet();
+            //allPoints = allPoints.Concat(toRemove.Select(p => new Point(p.X, p.Y + (int)hiddenTowerHeight))).ToHashSet();
 
-            //var topPixel = int.MaxValue;
-            foreach (var point in occupiedPoints)
-            {
-                var y = (imageTowerHeight - point.Y - 1) * PixelSize;
-                g.FillRectangle(FixedStoneColor, (point.X + 1) * PixelSize, y, PixelSize, PixelSize);
-                //if(y < topPixel)
-                //    topPixel = y;
-            }
-            form.Update(image, true);
-            //form.SetFocusTo(0, topPixel);
-        }
-
-        private void CreateNewImage()
-        {
-            g?.Dispose();
-            image?.Dispose();
-            if (occupiedPoints.Any())
-                imageTowerHeight = occupiedPoints.Max(x => x.Y) + 1;
-            imageTowerHeight += 1000;
-            image = new Bitmap((CaveWidth + 2) * PixelSize, (imageTowerHeight + 1) * PixelSize);
-            g = Graphics.FromImage(image);
+            occupiedPoints.RemoveWhere(x => x.Y < offset);
+            occupiedPoints = occupiedPoints.Select(x => new Point(x.X, x.Y - offset)).ToHashSet();
+            hiddenTowerHeight += offset;
         }
     }
 }
