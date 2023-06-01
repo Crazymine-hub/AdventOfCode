9aa0b5be0ac0dc538d89551270579e04d2a94c88
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2023-06-01 19:53:38 +0200

    Solved Day17 Part 2

--- AoC2022/Days/Day17.cs
+++ AoC2022/Days/Day17.cs
@@ -1,11 +1,14 @@
-﻿using AdventOfCode.Tools;
+﻿using AdventOfCode.Days.Tools.Day17;
+using AdventOfCode.Tools;
 using AdventOfCode.Tools.Visualization;
 using System;
 using System.Collections.Generic;
 using System.Data.OleDb;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AdventOfCode.Days
@@ -59,30 +62,43 @@ namespace AdventOfCode.Days
             }
         };
         private const int CaveWidth = 7;
+
         private HashSet<Point> occupiedPoints = new HashSet<Point>();
-        private ConsoleAssist ConsoleAssist = new ConsoleAssist();
+        private List<ShapePlacement> placementList = new List<ShapePlacement>();
+        private bool patternApplied = false;
 
         private long hiddenTowerHeight;
 
         public override string Solve(string input, bool part2)
         {
             _movement = input;
+            FallRocks(part2);
+
+            var towerHeight = GetTowerHeight() + hiddenTowerHeight;
+            Console.WriteLine();
+            return $"The Rock tower is {towerHeight} units tall";
+        }
+
+        private void FallRocks(bool part2)
+        {
             int shapeIndex = 0;
-            int sidewaysIndex = 0;
-            Point rockPosition = Point.Empty;
-            List<Point> currentShape = null;
+            int jetIndex = 0;
             bool movedByJet = true;
+
+            Point rockPosition;
+            List<Point> currentShape;
+
             SpawnRock(ref shapeIndex, out rockPosition, out currentShape);
-            uint rockCount = 1;
+            long rockCount = 1;
             var maxRocks = part2 ? 1_000_000_000_000 : 2022;
-            decimal progress = -1;
+            Regex expression = new Regex(@"(?<main>(?<v>;\d+)+?)(\k<main>)+", RegexOptions.Compiled);
             while (rockCount <= maxRocks)
             {
                 var nextPosition = new Point(rockPosition.X, rockPosition.Y);
                 if (movedByJet)
                 {
-                    bool moveLeft = _movement[sidewaysIndex++] == '<';
-                    if (sidewaysIndex >= _movement.Length) sidewaysIndex = 0;
+                    bool moveLeft = _movement[jetIndex++] == '<';
+                    if (jetIndex >= _movement.Length) jetIndex = 0;
                     nextPosition.Offset(moveLeft ? -1 : 1, 0);
                 }
                 else
@@ -96,29 +112,85 @@ namespace AdventOfCode.Days
                 }
                 else if (!movedByJet)
                 {
-                    SetGrid(currentShape, rockPosition);
-                    HideLowerCave();
-                    //RenderCaveSystem();
+                    SetGrid(currentShape, rockPosition, shapeIndex, jetIndex);
+                    //HideLowerCave();
                     SpawnRock(ref shapeIndex, out rockPosition, out currentShape);
-                    var newProgress = Math.Round(rockCount * 100 / (decimal)maxRocks, 3);
-                    if(newProgress > progress)
-                    {
-                        progress = newProgress;
-                        Console.CursorLeft = 0;
-                        Console.Write(progress.ToString("0.000"));
-                        Console.Write("% ");
-                        Console.Write(ConsoleAssist.GetNextProgressChar());
-                    }
                     rockCount++;
+                    rockCount += SearchPattern(maxRocks, rockCount);
                     movedByJet = false;
                 }
 
                 movedByJet = !movedByJet;
             }
+        }
 
-            var towerHeight = GetTowerHeight() + hiddenTowerHeight;
-            Console.WriteLine();
-            return $"The Rock tower is {towerHeight} units tall";
+        private long SearchPattern(long maxBlocks, long fallenBlocks)
+        {
+            if (patternApplied) return 0;
+            var endPlacement = placementList.Last();
+            var scanIndex = placementList.Count - 1;
+            var pattern = new List<ShapePlacement>() { endPlacement };
+            int offset = 0;
+
+            var startPoints = placementList.Where(x => x.PlacementCode == endPlacement.PlacementCode && x.ListIndex != endPlacement.ListIndex)
+                .Select(x => placementList.IndexOf(x))
+                .ToHashSet();
+            if (!startPoints.Any()) return 0;
+
+            while (scanIndex > 0)
+            {
+                scanIndex--;
+                offset++;
+                var scanItem = placementList[scanIndex];
+                pattern.Add(scanItem);
+                var points = placementList.Where(x => x.PlacementCode == scanItem.PlacementCode && x.JetIndex == scanItem.JetIndex && x.ListIndex != scanItem.ListIndex)
+                    .Select(x => x.ListIndex)
+                    .ToHashSet();
+                var newPoints = startPoints.Where(x => points.Contains(x - offset)).ToHashSet();
+                if (!newPoints.Any())
+                {
+                    if (startPoints.Count != 1 || offset <= 1 || startPoints.Single() != scanIndex) return 0;
+                    offset--;
+                    scanIndex++;
+                    pattern.RemoveAt(pattern.Count - 1);
+                    Console.WriteLine("PATTERN FOUND:");
+
+                    var addedHeight = GetPatternHeight(pattern);
+                    var addedBlocks = (long)pattern.Count;
+                    var remainingBlocks = checked((long)(maxBlocks - fallenBlocks));
+                    var stacks = remainingBlocks / addedBlocks;
+                    var remainder = remainingBlocks % addedBlocks;
+                    hiddenTowerHeight += addedHeight * stacks;
+                    patternApplied = true;
+                    return addedBlocks * stacks;
+                }
+                startPoints = newPoints;
+            }
+            return 0;
+        }
+
+        private long GetPatternHeight(List<ShapePlacement> pattern)
+        {
+            var beforePattern = placementList[placementList.Count - pattern.Count - 1];
+            var beforeTop = beforePattern.Height + _shapes[beforePattern.ShapeIndex].Max(x => x.Y);
+
+            var patternEnd = pattern.First();
+            var patternTop = patternEnd.Height + _shapes[patternEnd.ShapeIndex].Max(x => x.Y);
+
+            return patternTop - beforeTop;
+        }
+
+        private void GetShapeList()
+        {
+            using (var file = new StreamWriter(new FileStream(@"C:\Temp\Pattern.txt", FileMode.Create, FileAccess.ReadWrite)))
+            {
+                foreach (var item in placementList.Select(x => x.PlacementCode.ToString()).TakeWhile((x, i) => i < placementList.Count - 1))
+                {
+                    file.Write(item);
+                    file.Write(", ");
+                }
+                file.Write(placementList.Last().PlacementCode.ToString());
+            }
         }
 
         private void SpawnRock(ref int shapeIndex, out Point rockPosition, out List<Point> currentShape)
@@ -135,8 +207,9 @@ namespace AdventOfCode.Days
 
         private int GetTowerHeight() => occupiedPoints.Max(x => x.Y) + 1;
 
-        private void SetGrid(List<Point> shape, Point shapePosition)
+        private void SetGrid(List<Point> shape, Point shapePosition, int shapeIndex, int jetIndex)
         {
+            placementList.Add(new ShapePlacement(shapePosition.Y + hiddenTowerHeight, shapeIndex, shapePosition.X, placementList.Count, jetIndex));
             foreach (var point in shape)
                 occupiedPoints.Add(new Point(shapePosition.X + point.X, shapePosition.Y + point.Y));
         }
@@ -151,20 +224,6 @@ namespace AdventOfCode.Days
                 if (targetX < 0 || targetX >= CaveWidth || targetY < 0 || occupiedPoints.Contains(new Point(targetX, targetY))) return false;
             }
             return true;
-        }
-
-        private void HideLowerCave()
-        {
-            var offset = occupiedPoints.GroupBy(x => x.X).Select(x => new Point(x.Key, x.Max(p => p.Y))).Min(x => x.Y);
-            if (offset == 0) return;
-            //var oldHeight = GetTowerHeight() + hiddenTowerHeight;
-            //Console.WriteLine($"Height: {oldHeight}");
-            //var toRemove = occupiedPoints.Where(p => p.Y < hiddenHeightStep).ToHashSet();
-            //allPoints = allPoints.Concat(toRemove.Select(p => new Point(p.X, p.Y + (int)hiddenTowerHeight))).ToHashSet();
-
-            occupiedPoints.RemoveWhere(x => x.Y < offset);
-            occupiedPoints = occupiedPoints.Select(x => new Point(x.X, x.Y - offset)).ToHashSet();
-            hiddenTowerHeight += offset;
         }
     }
 }
---
+++ AoC2022/Days/Tools/Day17/ShapePlacement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Days.Tools.Day17
+{
+    [DebuggerDisplay("{X}|{Height}: {ShapeIndex} ({PlacementCode})")]
+    internal struct ShapePlacement
+    {
+        public int ListIndex { get; }
+        public long Height { get; }
+        public int ShapeIndex { get; }
+        public int X { get; }
+        public int PlacementCode { get; }
+        public int JetIndex { get; }
+
+        public ShapePlacement(long height, int shapeIndex, int x, int listIndex, int jetIndex)
+        {
+            Height = height;
+            ShapeIndex = shapeIndex;
+            X = x;
+            PlacementCode = x << 4;
+            PlacementCode |= shapeIndex;
+            ListIndex = listIndex;
+            JetIndex = jetIndex;
+        }
+    }
+}
