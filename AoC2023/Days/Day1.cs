b0593ac62b0329aad242a60467519c4396002c0e
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2023-11-30 23:28:01 +0100

    Initialization for 2023

---
+++ AoC2023/Days/Day1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Days
+{
+    public class Day1: DayBase
+    {
+        public override string Title => "TestDay";
+
+        public override string Solve(string input, bool part2)
+        {
+            return "TEST";
+        }
+    }
+}
