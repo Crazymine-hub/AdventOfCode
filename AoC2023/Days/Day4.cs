efd5166e9f994426fd272f726838f5d0b90091cb
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2023-12-04 09:49:53 +0100

    Added Day4 Part2

--- AoC2023/Days/Day4.cs
+++ AoC2023/Days/Day4.cs
@@ -13,37 +13,61 @@ public class Day4: DayBase
 
     public override string Solve(string input, bool part2)
     {
-        if(part2) return Part2UnavailableMessage;
         var cards = GetLines(input)
             .Select(x => x.Split(":", options: splitOptions)[1]
                 .Split("|", options: splitOptions)
                 .Select(x => x.Split(" ", options: splitOptions))
                 .ToList())
+            .Select(x => new CardInfo(x))
             .ToList();
 
         int totalPoints = 0;
-        foreach(var card in cards)
+        for(int c = 0; c < cards.Count; c++)
         {
-            var numbers = card.SelectMany(x => x).ToList();
-            for(int i = 0; i < numbers.Count; ++i)
-            {
-                Console.Write(numbers[i].ToString().PadRight(3));
-                if(i > 0 && i == numbers.Count % 2)
-                    Console.Write("| ");
-            }
-            int winNumbers = card[1].Count(x => card[0].Contains(x));
+            var card = cards[c];
+            Console.Write($"Card {c,-5} Copies: {card.Amount,-15}: ");
+            foreach(var number in card.Card[0])
+                Console.Write($"{number,-3}");
+            Console.Write("|");
+            foreach(var number in card.Card[1])
+                Console.Write($"{number,3}");
 
-            Console.Write($"=> Matches: {winNumbers,-3}");
-            if(winNumbers == 0)
+
+
+            int winNumbers = card.Card[1].Count(x => card.Card[0].Contains(x));
+            Console.Write($" => Matches: {winNumbers,-3}");
+
+            if(part2)
             {
-                Console.WriteLine($" Points: 0");
-                continue;
+                for(int n = c + 1; n <= c + winNumbers; n++)
+                    cards[n].Amount += card.Amount;
+                Console.WriteLine();
             }
-            var points = (int)Math.Pow(2, winNumbers - 1);
-            Console.WriteLine($" Points: {points}");
-            totalPoints += points;
+            else
+            {
+                if(winNumbers == 0)
+                {
+                    Console.WriteLine($" Points: 0");
+                    continue;
+                }
+                var points = (int)Math.Pow(2, winNumbers - 1);
+                Console.WriteLine($" Points: {points}");
+                totalPoints += points;
+            }
         }
 
+        if(part2)
+            return $"Total Cards {cards.Sum(x => x.Amount)}";
         return $"Total Points: {totalPoints}";
     }
+}
+
+internal class CardInfo
+{
+    public List<string[]> Card { get; set; }
+    public long Amount { get; set; } = 1;
+    public long Processed { get; set; } = 0;
+
+    public CardInfo(List<string[]> card) =>
+        Card = card;
 }
