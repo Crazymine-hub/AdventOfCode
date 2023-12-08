1ef9f378125d65f1c970d9798065086bd0db4b1c
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2023-12-08 17:30:21 +0100

    Added Day 5 Part2

--- AoC2023/Days/Day5.cs
+++ AoC2023/Days/Day5.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using AdventOfCode.Tools;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO.MemoryMappedFiles;
 using System.Linq;
@@ -16,11 +18,10 @@ internal partial class Day5: DayBase
 
     public override string Solve(string input, bool part2)
     {
-        if(part2) return Part2UnavailableMessage;
-
         var maps = GetGroupedLines(input);
         var data = InitSeedData(maps[0], part2);
 
+        var displayList = Enumerable.Range(0, 100).ToList();
         foreach(var map in maps.Skip(1))
         {
             var ranges = GetLines(map);
@@ -28,6 +29,7 @@ internal partial class Day5: DayBase
             var fromCol = header.Groups["srcCol"].Value;
             var toCol = header.Groups["targetCol"].Value;
 
+            var replacements = Enumerable.Repeat(' ', 100).ToList();
             foreach(var range in ranges.Skip(1))
             {
                 var rangeDefinimtion = range
@@ -36,26 +38,84 @@ internal partial class Day5: DayBase
                     .ToList();
                 var start = rangeDefinimtion[1];
                 var end = start + rangeDefinimtion[2] - 1;
-                foreach(var startRange in data[fromCol])
+                if(TestMode)
                 {
-                    if(startRange.Start < start || startRange.Start > end) continue;
-                    data[toCol].Add(new LongRange(rangeDefinimtion[0] + (startRange.Start - start), 0));
-                    startRange.Transferred = true;
+                    replacements[(int)start] = '\\';
+                    replacements[(int)end] = '/';
+                    if(start == end)
+                        replacements[(int)end] = 'X';
                 }
+                if(part2)
+                    HandlePart2Values(data, fromCol, toCol, rangeDefinimtion[0], start, end);
+                else
+                    HandlePart1Values(data, fromCol, toCol, rangeDefinimtion[0], start, end);
             }
             data[toCol].AddRange(data[fromCol].Where(x => !x.Transferred).Select(x => new LongRange(x.Start, x.End)));
+            if(TestMode)
+            {
+                Console.WriteLine(map);
+                Console.WriteLine(string.Concat(displayList.Select(x => x % 10 == 0 ? '|' : ' ')));
+                Console.WriteLine(string.Concat(displayList.Select(x => data[fromCol].Exists(y => y.ContainsValue(x)) ? "#" : "-")));
+                Console.WriteLine(string.Concat(replacements));
+                Console.WriteLine(string.Concat(displayList.Select(x => data[toCol].Exists(y => y.ContainsValue(x)) ? "#" : "-")));
+            }
         }
 
+
+        Console.WriteLine();
+        Console.WriteLine(string.Join(" | ", data.Select(y => string.Empty.PadLeft(14, '='))));
         Console.WriteLine(string.Join(" | ", data.Keys.Select(x => x.PadLeft(14))));
+        Console.WriteLine(string.Join(" | ", data.Select(y => string.Empty.PadLeft(14, '='))));
         for(int i = 0; data.Any(x => i < x.Value.Count); i++)
         {
             Console.WriteLine(string.Join(" | ", data.Select(y =>
                ((y.Value.ElementAtOrDefault(i)?.Start)?.ToString() ?? string.Empty).PadLeft(14))));
+            if(part2)
+            {
+                Console.WriteLine(string.Join(" | ", data.Select(y =>
+                   ((y.Value.ElementAtOrDefault(i)?.End)?.ToString() ?? string.Empty).PadLeft(14))));
+                Console.WriteLine(string.Join(" | ", data.Select(y => string.Empty.PadLeft(14, '-'))));
+            }
         }
 
         long lowestLocation = data["location"].Min(x => x.Start);
 
         return $"Closest location: {lowestLocation}";
+    }
+
+
+    private static void HandlePart1Values(Dictionary<string, List<LongRange>> data, string fromCol, string toCol, long newOrigin, long start, long end)
+    {
+        for(int i = 0; i < data[fromCol].Count; ++i)
+        {
+            var startRange = data[fromCol][i];
+            if(startRange.Start < start || startRange.Start > end) continue;
+            var newValue = newOrigin + (startRange.Start - start);
+            data[toCol].Add(new LongRange(newValue, newValue));
+            startRange.Transferred = true;
+        }
+    }
+    private void HandlePart2Values(Dictionary<string, List<LongRange>> data, string fromCol, string toCol, long newOrigin, long start, long end)
+    {
+        for(int i = 0; i < data[fromCol].Count; ++i)
+        {
+            var startRange = data[fromCol][i];
+            if(startRange.Transferred || end < startRange.Start || start > startRange.End) continue;
+
+            LongRange transfer = new LongRange(start < startRange.Start ? startRange.Start : start,
+                end > startRange.End ? startRange.End : end);
+            if(transfer.Start != startRange.Start)
+                data[fromCol].Add(new LongRange(startRange.Start, transfer.Start - 1));
+            if(transfer.End != startRange.End)
+                data[fromCol].Add(new LongRange(transfer.End + 1, startRange.End));
+
+            var offset = transfer.Start - start;
+            var length = transfer.End - transfer.Start;
+            transfer.Start = newOrigin + offset;
+            transfer.End = transfer.Start + length;
+            startRange.Transferred = true;
+            data[toCol].Add(transfer);
+        }
     }
 
     private static Dictionary<string, List<LongRange>> InitSeedData(string seeds, bool part2)
@@ -72,12 +132,18 @@ internal partial class Day5: DayBase
             { "location", [] }
         };
 
-        foreach(Match seed in NumberRegex().Matches(seeds))
-            seedData["seed"].Add(new LongRange(long.Parse(seed.Value), 0));
+        var seedMatches = NumberRegex().Matches(seeds).Chunk(part2 ? 2 : 1);
+        foreach(Match[] seed in seedMatches)
+        {
+            var startValue = long.Parse(seed[0].Value);
+            var endValue = startValue + (part2 ? long.Parse(seed[1].Value) : 1) - 1;
+            seedData["seed"].Add(new LongRange(startValue, endValue));
+        }
 
         return seedData;
     }
 
+    [DebuggerDisplay("{Start}-{End} T:{Transferred}")]
     private sealed class LongRange
     {
         public long Start { get; set; }
@@ -94,6 +160,8 @@ internal partial class Day5: DayBase
         public static implicit operator (long start, long end)(LongRange value) => (value.Start, value.End);
         public static implicit operator LongRange((long start, long end) value) => new
             LongRange(value.start, value.end);
+
+        public bool ContainsValue(long value) => value >= Start && value <= End;
     }
 
     [GeneratedRegex(@"\d+")]
