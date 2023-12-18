15a63b44a57e8e946c03621790d587ca309f77a6
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2023-12-18 22:24:15 +0100

    Added Day10 part 2

--- AoC2023/Days/Day10.cs
+++ AoC2023/Days/Day10.cs
@@ -5,7 +5,9 @@ using AdventOfCode.Tools.DynamicGrid;
 using AdventOfCode.Tools.Pathfinding;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Diagnostics;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Numerics;
 using System.Text;
@@ -18,8 +20,6 @@ public class Day10: DayBase
 
     public override string Solve(string input, bool part2)
     {
-        if(part2) return Part2UnavailableMessage;
-
         //Mapping TraceChars via constants
         DynamicGrid<DirectionalNode> grid = DynamicGrid.GenerateFromInput(input, (value, x, y) =>
         new DirectionalNode(x, y,
@@ -76,17 +76,76 @@ public class Day10: DayBase
 
         path.Add(startNode);
 
-        TraversePath(path);
-        TraversePath(path.Reverse<DirectionalNode>());
+        TraversePath(path, true);
+        TraversePath(path.Reverse<DirectionalNode>().ToList(), false);
 
         var furthestNode = grid.MaxBy(x => x.Value.StepsFromStart).Value;
 
-        Console.WriteLine(grid.GetStringRepresentation((x, _, _) => x == furthestNode ? "X" : TraceChars.paths[(int)x.Direction].ToString()));
+        Console.WriteLine(RenderGrid(grid, startNode, furthestNode));
+        if(part2)
+        {
+            Console.WriteLine("  X|Y   Dir.      A In | B Out ");
+            Console.WriteLine(string.Join(Environment.NewLine, path.Select(x => $"{x.X,3}|{x.Y,-3} ({TraceChars.paths[(int)x.Direction]}) {x.SideA,10} | {x.SideB}")));
+
+            var nonLoop = grid.Select(x => x.Value).Where(x => !x.Direction.HasFlag(TraceChars.Direction.Bold)).ToList();
+
+            var topNode = path.MinBy(x => x.Y);
+            bool aIsInside = topNode.SideB.HasFlag(TraceChars.Direction.Up);
+            if(!aIsInside && !topNode.SideA.HasFlag(TraceChars.Direction.Up))
+                throw new InvalidOperationException($"The chosen Node {topNode} should have a UP Side. but somehow does not");
+
+            Console.WriteLine("Determining inside Points....");
+            var taskList = new List<Task>();
+            foreach(var node in nonLoop)
+            {
+                var task = new Task(() => DetermineNode(node, path, aIsInside), CancellationToken);
+                taskList.Add(task);
+                task.Start();
+            }
+
+            Task.WaitAll(taskList.ToArray());
+            Console.WriteLine(RenderGrid(grid, startNode, furthestNode));
+            return $"The grid encloses {grid.Count(x => x.Value.IsInside)} Nodes.";
+        }
         return $"The furthest node is {furthestNode.StepsFromStart} steps away from start";
     }
 
-    private static void TraversePath(IEnumerable<DirectionalNode> path)
+    private static string RenderGrid(DynamicGrid<DirectionalNode> grid, DirectionalNode startNode, DirectionalNode furthestNode)
     {
+        return grid.GetStringRepresentation((x, _, _) =>
+        {
+            if(x == furthestNode)
+                return "X";
+            else if(x == startNode)
+                return "S";
+            else if(!x.Direction.HasFlag(TraceChars.Direction.Bold) && x.IsInside)
+                return "I";
+            else
+                return TraceChars.paths[(int)x.Direction].ToString();
+        });
+    }
+
+    private void DetermineNode(DirectionalNode node, List<DirectionalNode> wall, bool aIsInside)
+    {
+        var closestNode = wall.Where(x => x.X == node.X || x.Y == node.Y).MinBy(x => x.GetDistanceTo(node));
+        var direction = Rotate(GetDirection(node, closestNode), 2);
+
+        if(aIsInside)
+            node.IsInside = closestNode.SideA.HasFlag(direction);
+        else
+            node.IsInside = closestNode.SideB.HasFlag(direction);
+    }
+
+    private static void TraversePath(List<DirectionalNode> path, bool updateSides)
+    {
+
+        TraceChars.Direction fromDirection = GetDirection(path[0], path[1]);
+        path[0].Direction = fromDirection;
+        fromDirection = GetDirection(path[^1], path[^2]);
+        path[0].Direction |= fromDirection;
+
+
+
         int steps = 0;
         foreach(var node in path)
         {
@@ -94,6 +153,75 @@ public class Day10: DayBase
             if(node.StepsFromStart == -1 || node.StepsFromStart > steps)
                 node.StepsFromStart = steps;
             steps++;
+
+            if(updateSides)
+            {
+                var toDirection = node.Direction & ~(fromDirection | TraceChars.Direction.Bold);
+                var rotateCount = 0;
+                var upBit = TraceChars.Direction.Up;
+                int rotationCount = 0;
+                while(upBit != fromDirection)
+                {
+                    upBit = Rotate(upBit, 1);
+                    rotationCount++;
+                    if(rotationCount > 3)
+                        throw new InvalidOperationException("Unable to find shift amount for Up");
+                }
+
+                var leftSide = Rotate(TraceChars.Direction.Left, rotationCount);
+                var rightSide = Rotate(TraceChars.Direction.Right, rotationCount);
+                var openSides = TraceChars.Direction.AllDriections & ~(fromDirection | toDirection);
+
+                node.SideA = leftSide & openSides;
+                node.SideB = rightSide & openSides;
+                if((node.SideA | node.SideB) != openSides)
+                {
+                    if(node.SideA == TraceChars.Direction.None)
+                        node.SideB |= openSides;
+                    else
+                        node.SideA |= openSides;
+                }
+
+                fromDirection = Rotate(toDirection, 2);
+            }
         }
+    }
+
+    private static TraceChars.Direction GetDirection(DirectionalNode from, DirectionalNode to)
+    {
+        TraceChars.Direction fromDirection;
+        var xStart = to.X - from.X;
+        var yStart = to.Y - from.Y;
+
+        if(xStart == 0)
+        {
+            if(yStart < 0)
+                fromDirection = TraceChars.Direction.Up;
+            else if(yStart > 0)
+                fromDirection = TraceChars.Direction.Down;
+            else throw new InvalidOperationException("Both nodes seem to be on top of each other");
+        }
+        else if(xStart < 0)
+            fromDirection = TraceChars.Direction.Left;
+        else
+            fromDirection = TraceChars.Direction.Right;
+        return fromDirection;
+    }
+
+    private static TraceChars.Direction Rotate(TraceChars.Direction direction, int amount)
+    {
+        for(int i = 0; i < (amount % 4); ++i)
+        {
+            direction = direction switch
+            {
+                TraceChars.Direction.Up => TraceChars.Direction.Right,
+                TraceChars.Direction.Right => TraceChars.Direction.Down,
+                TraceChars.Direction.Down => TraceChars.Direction.Left,
+                TraceChars.Direction.Left => TraceChars.Direction.Up,
+                _ => throw new NotSupportedException($"The Direction {direction} is not supported for Rotation")
+            };
+        }
+
+        return direction;
     }
 }
--- AoC2023/Days/Tools/Day10/DirectionalNode.cs
+++ AoC2023/Days/Tools/Day10/DirectionalNode.cs
@@ -12,6 +12,11 @@ public class DirectionalNode: BaseNode
     public TraceChars.Direction Direction { get; set; }
     public int StepsFromStart { get; set; } = -1;
 
+    public TraceChars.Direction SideA { get; set; } = TraceChars.Direction.None;
+    public TraceChars.Direction SideB { get; set; } = TraceChars.Direction.None;
+
+    public bool IsInside { get; set; }
+
     public DirectionalNode(int x, int y, TraceChars.Direction direction) : base(x, y)
     {
         Direction = direction;
