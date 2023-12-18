393809ecacfd3b60146ee75192f108f2cba03007
Crazymine-hub <60275257+Crazymine-hub@users.noreply.github.com>
2023-12-19 00:53:51 +0100

    Added Day11 Part 2

--- AoC2023/Days/Day11.cs
+++ AoC2023/Days/Day11.cs
@@ -14,31 +14,29 @@ public class Day11: DayBase
 
     public override string Solve(string input, bool part2)
     {
-        if(part2) return Part2UnavailableMessage;
-
-        int index = 1;
         var universe = DynamicGrid.GenerateFromInput(input, x => x == '#');
-
-
-
-        RenderUniverse(universe);
-        Expand(universe);
-        Console.WriteLine();
-        Console.WriteLine("EXPANSION");
-        Console.WriteLine();
+        var (rowHeights, columnWidths) = Expand(universe, part2);
         RenderUniverse(universe);
         
         
         var galaxies = universe.Where(x => x.Value).ToList();
-        var distanceSum = 0;
+        long distanceSum = 0;
 
         for(int i = 0; i < galaxies.Count; i++)
-            for(int j = i; j < galaxies.Count; j++)
+            for(int j = i + 1; j < galaxies.Count; j++)
             {
-                var a = galaxies[i];
-                var b = galaxies[j];
+                var a = galaxies[i].AsPoint();
+                var b = galaxies[j].AsPoint();
 
-                var distance = VectorAssist.ManhattanDistance(a.AsPoint(), b.AsPoint());
+                var min = Math.Min(a.X, b.X);
+                var max = Math.Max(a.X, b.X);
+                var width = columnWidths.Where((x, i) => i >= min && i < max).Sum();
+                min = Math.Min(a.Y, b.Y);
+                max = Math.Max(a.Y, b.Y);
+                var height = rowHeights.Where((x, i) => i >= min && i < max).Sum();
+
+
+                var distance = width + height;
                 distanceSum += distance;
             }
 
@@ -46,9 +44,16 @@ public class Day11: DayBase
     }
 
 
-    private void Expand(DynamicGrid<bool> universe)
+    private (List<long> rowHeights, List<long> columnWidths) Expand(DynamicGrid<bool> universe, bool part2)
     {
         List<bool> columnHasGalaxy = [.. Enumerable.Repeat(false, universe.XDim)];
+        List<long> columnWidth = [.. Enumerable.Repeat(1L, universe.XDim)];
+        List<long> rowHeight = [.. Enumerable.Repeat(1L, universe.XDim)];
+
+        var part2Expansion = TestMode ? 10 : 1_000_000;
+        const int part1Expansion = 2;
+
+
         for(int y = 0; y < universe.YDim; ++y)
         {
             bool rowHasGalaxy = false;
@@ -59,20 +64,13 @@ public class Day11: DayBase
             }
 
             if(!rowHasGalaxy)
-            {
-                universe.InsertRow(y);
-                y++;
-            }
+                rowHeight[y] = part2 ? part2Expansion : part1Expansion;
         }
 
         for(int i = 0; i < columnHasGalaxy.Count; ++i)
             if(!columnHasGalaxy[i])
-            {
-                universe.InsertColumn(i);
-                columnHasGalaxy.Insert(i, false);
-                ++i;
-            }
-
+                columnWidth[i] = part2 ? part2Expansion : part1Expansion;
+        return (rowHeight, columnWidth);
     }
     private void RenderUniverse(DynamicGrid<bool> universe) =>
         Console.WriteLine(universe.GetStringRepresentation((val, _, _) => (val ? '#' : '.').ToString()));
